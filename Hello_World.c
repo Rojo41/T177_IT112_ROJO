b7671eee4bf404ad23c9d9a5e2fe1fe354968d1b
Rojo41 <115506131+Rojo41@users.noreply.github.com>
2022-10-11 17:33:36 +0800

    Add files via upload

---
+++ Hello_World.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#conclude <conio.h>
+//C Programming Language.
+int main()
+{
+    //My name.
+
+    printf("Rey Anthony Rojo\n");
+    printf("I love coding!\n");
+
+    //variables.
+
+    int apples = 50;
+    float money = 400.2112;
+    char middle = 'P';
+
+    printf("There are %d apples \n", apples);
+    printf("I have %f pesos\n", money);
+    printf("My middle initial is %c", middle);
+
+    getch();
+    return 0;
+}
